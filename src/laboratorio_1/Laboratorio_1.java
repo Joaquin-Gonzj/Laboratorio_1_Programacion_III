a6d3c525c478928737cf16ca042835ac8ec7a128
Quincho <cortezgonzalez@gmail.com>
2020-05-20 21:50:00 -0600

    Repositorio Local de NetBeans. Laboratorio 01 Programacion III

---
+++ src/laboratorio_1/Laboratorio_1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio_1;
+
+/**
+ *
+ * @author usuario
+ */
+public class Laboratorio_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Repositorio();
+        
+    }
+    
+    
+    public static void Repositorio ( ) {
+        
+        System.out.println("Hola Mundo");
+        
+    }
+    
+}
